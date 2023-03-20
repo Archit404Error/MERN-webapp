a0ff97ca1e63d4fe07520384f83aee823fc5eb8b
Archit404Error <4architmehta@gmail.com>
2023-03-20 00:15:28 -0400

    Add swagger tags

--- backend/src/customers/views.ts
+++ backend/src/customers/views.ts
@@ -9,10 +9,12 @@ import { successJson, errorJson } from "../utils/jsonResponses";
 const customerRouter = Router();
 
 customerRouter.get("/", async (req, res) => {
+  // #swagger.tags = ['Customers']
   res.status(200).send(await CustomerController.getCustomers());
 });
 
 customerRouter.get("/:id", async (req, res) => {
+  // #swagger.tags = ['Customers']
   const id = new mongoose.Types.ObjectId(req.params.id);
   res
     .status(200)
@@ -20,6 +22,7 @@ customerRouter.get("/:id", async (req, res) => {
 });
 
 customerRouter.post("/", async (req, res) => {
+  // #swagger.tags = ['Customers']
   const { name, age, title, company } = req.body;
   res
     .status(201)
@@ -31,6 +34,7 @@ customerRouter.post("/", async (req, res) => {
 });
 
 customerRouter.put("/updateName/:id", async (req, res) => {
+  // #swagger.tags = ['Customers']
   const id = new mongoose.Types.ObjectId(req.params.id);
   const name = req.body.name;
   res
@@ -39,6 +43,7 @@ customerRouter.put("/updateName/:id", async (req, res) => {
 });
 
 customerRouter.post("/resetAges", async (req, res) => {
+  // #swagger.tags = ['Customers']
   const numResets = await CustomerController.resetAges();
   res.status(200).send(successJson(numResets));
 });
